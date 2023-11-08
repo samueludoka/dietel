427b522580caa39de36bebebef362cf22003cbde
samuel udoka <shedrachsamuel4@gmail.com>
2023-11-09 00:15:57 +0100

    update

---
+++ src/chapter6Exercises/DisplayNumberPattern.java
@@ -0,0 +1,16 @@
+package chapter6Exercises;
+
+public class DisplayNumberPattern {
+
+    public static void displayPattern(boolean number){
+        for (int i = 0; i <= 3; i++) {
+            System.out.println();
+            for (int j = i; j >= 0; j--) {
+                System.out.print(1+i);
+
+            }
+
+        }
+
+    }
+}
--- src/chapter7Assignment/ShoppingList2Display.java
+++ src/chapter7Assignment/ShoppingList2Display.java
@@ -1,8 +1,6 @@
 package chapter7Assignment;
 
-import chapter7Assignment.ShoppingList2;
-
-public class ShopDisplay {
+public class ShoppingList2Display {
     public static void main(String[] args) {
         ShoppingList2 shoppingList2 = new ShoppingList2();
         shoppingList2.shop();
