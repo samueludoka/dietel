790da2e53f1d1566ad434fb2bbe4e85a53a27808
samuel udoka <shedrachsamuel4@gmail.com>
2024-05-10 09:12:24 +0100

    first commit

---
+++ src/LeetCode/Exercise1.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+public class Exercise1 {
+
+    public long maximumHappinessSum(int[] happiness, int k){
+        int sum = 0;
+        for (int count = 0; count < happiness.length; count++) {
+            sum =+ count;
+        }
+        return sum;
+    }
+    public int[] twoSum(int[] nums, int target){
+        int targe = 0;
+        int tanget = 0;
+        int[] numb = new int[2];
+        for (int count = 0; count < nums.length; count++) {
+            targe = nums[0];
+            tanget = nums[1];
+        }
+        return numb;
+    }
+
+}
